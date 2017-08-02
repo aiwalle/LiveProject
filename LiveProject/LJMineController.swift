c64a79ebe1f9f26823255ffcc902ee81151827d3
aiwalle <wallelj0909@gmail.com>
2017-08-02 15:53:25 +0800

    update 添加了全屏pop及粒子动画

--- LiveProject/LJMineController.swift
+++ LiveProject/LJMineController.swift
@@ -12,24 +12,58 @@ class LJMineController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.backgroundColor = UIColor.white
+        
+        startEmittering()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        startEmittering()
+        stopEmittering()
     }
-    */
+
+    
+    func startEmittering() {
+        // 粒子动画
+        let emmiter = CAEmitterLayer()
+        emmiter.emitterPosition = CGPoint(x: view.bounds.width * 0.5, y: view.bounds.height * 0.5)
+        emmiter.preservesDepth = true
+        
+        var cells = [CAEmitterCell]()
+        
+        for i in 0..<10 {
+            let cell = CAEmitterCell()
+            cell.birthRate = 10
+            cell.lifetime = 3
+            cell.lifetimeRange = 2
+            cell.scale = 0.7
+            cell.scaleRange = 0.2
+            cell.emissionLongitude = CGFloat(-Double.pi / 2)
+            cell.emissionRange = CGFloat(Double.pi / 12)
+            cell.velocity = 50
+            cell.velocityRange = 20
+            cell.contents = UIImage(named: "good\(i)_30x30")?.cgImage
+            cell.birthRate = 2
+            cell.spin = CGFloat(.pi / 2.0)
+            cell.spinRange = CGFloat(.pi / 4.0 )
+            
+            cells.append(cell)
+        }
+        emmiter.emitterCells = cells
+        view.layer.addSublayer(emmiter)
+        
+    }
+    
+    func stopEmittering() {
+        /*
+         for layer in view.layer.sublayers! {
+         if layer.isKind(of: CAEmitterLayer.self) {
+         layer.removeFromSuperlayer()
+         }
+         }
+         */
+        view.layer.sublayers?.filter({ $0.isKind(of: CAEmitterLayer.self)}).first?.removeFromSuperlayer()
+    }
+    
 
 }
--- LiveProject/LJNavigationCotroller.swift
+++ LiveProject/LJNavigationCotroller.swift
@@ -12,26 +12,32 @@ class LJNavigationCotroller: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 全屏Pop
+        
         
         // 通过运行时获取到gesture对应的target和action
+        /*
         var count : UInt32 = 0
         let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
         for i in 0..<count {
             let ivar = ivars[Int(i)]
             let nameP = ivar_getName(ivar)
             let name = String(cString: nameP!)
-            print(name)
         }
+        */
         
-        
-        guard let interactive = interactivePopGestureRecognizer else {
+        // 全屏Pop
+        guard let value = interactivePopGestureRecognizer?.value(forKeyPath: "_targets") as? [NSObject] else {
             return
         }
+        guard let objc = value.first else {
+            return
+        }
+        let target = objc.value(forKeyPath: "target")
+        let action = Selector(("handleNavigationTransition:"))
         
-//        let panGes = UIPanGestureRecognizer(target: <#T##Any?#>, action: <#T##Selector?#>)
-//        
-//        view.addGestureRecognizer(interactive)
+        let panGes = UIPanGestureRecognizer(target: target, action: action)
+        
+        view.addGestureRecognizer(panGes)
         
     }
     
