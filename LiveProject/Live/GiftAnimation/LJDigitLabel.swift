40aa8c77118370890fd258b2d51c1c60fc6b2a41
aiwalle <wallelj0909@gmail.com>
2017-09-02 19:26:34 +0800

    add 添加了giftanimation

---
+++ LiveProject/Live/GiftAnimation/LJDigitLabel.swift
@@ -0,0 +1,22 @@
+//
+//  LJDigitLabel.swift
+//  LiveProject
+//
+//  Created by liang on 2017/9/1.
+//  Copyright © 2017年 liang. All rights reserved.
+//
+
+import UIKit
+
+class LJDigitLabel: UILabel {
+
+    override func drawText(in rect: CGRect) {
+        
+        
+        super.drawText(in: rect)
+        
+        super.drawText(in: rect)
+    }
+ 
+
+}
---
+++ LiveProject/Live/Model/LJGiftAnimationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LJGiftAnimationModel.swift
+//  LiveProject
+//
+//  Created by liang on 2017/9/2.
+//  Copyright © 2017年 liang. All rights reserved.
+//
+
+import UIKit
+
+class LJGiftAnimationModel: NSObject {
+
+}
